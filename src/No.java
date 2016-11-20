e979947c845c04bb06a4455e3732633d91927d87
Guilherme Maeda <gmaeda6@gmail.com>
2016-11-20 21:03:18 -0200

    Update No.java

--- src/No.java
+++ src/No.java
@@ -1,12 +1,12 @@
 
 public class No {
- static No esquerda;
- static No direita;
- static String answer;
- static String newAnimal;
- static String valor;
- static String root;
+ public No left;
+ public No right;
+ 
+ public String value;
+ public String answer;
  
  
  
 }
+
